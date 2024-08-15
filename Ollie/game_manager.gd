557a0b5dfb21b695123e7c8d5012e2c14a7c97e6
Marcel Buchner <marcel.grubmueller@gmx.at>
2024-08-15 16:55:50 +0200

    Das ist ein Update

---
+++ Ollie/game_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var coins = 0
+var lives = 3
+@onready var coins_label = $"../UI/Panel/CoinsLabel"
+@export var hearts : Array[Node]
+
+func add_coins():
+	coins += 1
+	print(coins)
+	coins_label.text = "Coins: " + str(coins)
+	
+func decrease_healh():
+	lives -= 1
+	print(lives)
+	for h in 3:
+		if(h < lives):
+			hearts[h].show()
+		else:
+			hearts[h].hide()
+	if(lives == 0):
+		get_tree().reload_current_scene()
---
+++ Ollie/wood_enemy.gd
@@ -0,0 +1,28 @@
+extends RigidBody2D
+@onready var game_manager = %GameManager
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if(body.name == "Ollie"):
+		var y_delta = position.y - body.position.y
+		var x_delta = body.position.x - position.x
+		if(y_delta > 30):
+			print("enemy defeated")
+			queue_free()
+			body.jump()
+		else:
+			print("Lost Life")
+			game_manager.decrease_healh()
+			if(x_delta > 0):
+				body.jump_slide(500)
+			else:
+				body.jump_slide(-500)
