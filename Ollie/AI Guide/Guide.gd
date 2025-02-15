c380dd9bb912cec46e33c790d0ed2b497b8d98b6
Hiros53 <146209053+Hiros53@users.noreply.github.com>
2025-02-15 16:04:05 +0100

    Ai update

---
+++ Ollie/AI Guide/Guide.gd
@@ -0,0 +1,245 @@
+extends CharacterBody2D
+
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -930.0
+
+const ACCELERATION_TIME = 0.04 # time to reach full speed on the ground
+const DECELERATION_TIME = 0.04 # time to fully stop on the ground
+const AIR_INERTIA_MULTIPLIER = 3.0 # inertia multiplier when in the air
+const COYOTE_TIME = 0.2 # time in seconds to allow jumping after leaving the ground
+const WALLJUMP_GRACE = 0.3 #time after walljump where pressing the opposite way doesnt do anything
+
+@onready var sprite_2d = $Sprite2D
+@onready var sfx_jump = $sfx_jump
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var direction
+var cur_direction = 1
+var acceleration = SPEED / ACCELERATION_TIME
+var deceleration = SPEED / DECELERATION_TIME
+var has_walljump = false
+var has_backwards_walljump = true
+var does_spcial_action = false
+var does_moveable_action = false
+var is_in_air = false
+var can_shoot = true
+
+var coyote_timer = 0.0 # tracks how long we've been off the ground
+var grace_timer = 0.0 # tracks how long since last wall jump
+var in_portal = false #track if the player is in the portal area
+var transition
+var current_portal: Area2D = null
+
+var power_state: GameManager.PossiblePowers = GameManager.PossiblePowers.NORMAL
+
+func jump():
+	velocity.y = JUMP_VELOCITY
+func jump_slide(x):
+	velocity.y = JUMP_VELOCITY
+	velocity.x = x
+
+func _ready():
+	add_to_group("Player")
+	power_state = Global.cur_power
+	call_deferred("_find_spawn_container")
+	call_deferred("_find_transition")
+	$hitbox_Timer_Bonk.one_shot = true
+	$gun_out_timer.one_shot = true
+	
+func _find_spawn_container():
+	var scene_objects = get_tree().current_scene.get_node("SceneObjects")
+	var spawn_container = scene_objects.get_node("Portals_and_Spawns")
+
+	
+	var spawn_points = spawn_container.get_children()
+
+	for spawn_point in spawn_points:
+		if spawn_point is SpawnPoint and spawn_point.spawn_index == Global.target_spawnpoint:
+			position = spawn_point.global_position
+			position.x = position.x + 100
+			break
+			
+func _find_transition():
+	var scene_objects = get_tree().current_scene.get_node("SceneObjects")
+	var ollie_object = scene_objects.get_node("Ollie")
+	var camera_object = ollie_object.get_node("Camera2D")
+	transition = camera_object.get_node("Transition")
+	# transition.FadeIn()
+
+func _physics_process(delta):
+	is_in_air = not is_on_floor()
+	var inertia_multiplier = AIR_INERTIA_MULTIPLIER if is_in_air else 1.0
+	
+	# Determine the acceleration and deceleration based on whether the character is in the air
+	var adjusted_acceleration = acceleration / inertia_multiplier
+	var adjusted_deceleration = deceleration / inertia_multiplier
+	
+	var wallslide = is_on_wall_only() and (has_walljump or has_backwards_walljump)
+	# Handle coyote time
+	if is_on_floor():
+		coyote_timer = COYOTE_TIME # Reset the coyote timer if on the ground
+	else:
+		coyote_timer = max(coyote_timer - delta, 0) # Decrease the timer if in the air
+	
+	# direction = Input.get_axis("left", "right")
+	
+	#animation corner:
+	if not does_spcial_action and not does_moveable_action:
+		match power_state:
+			GameManager.PossiblePowers.NORMAL:
+				if not is_on_floor():
+					sprite_2d.animation = "jumping"
+				elif direction != 0:
+					sprite_2d.animation = "running"
+				else:
+					sprite_2d.animation = "default"
+				
+			GameManager.PossiblePowers.PIRATE:
+				if not is_on_floor():
+					sprite_2d.animation = "Pirate_jump"
+				elif (direction != 0):
+					sprite_2d.animation = "Pirate_run"
+				else:
+					sprite_2d.animation = "Pirate_idle"
+			GameManager.PossiblePowers.CAVEMAN:
+				if not is_on_floor():
+					sprite_2d.animation = "Caveman_jump"
+				elif (direction != 0):
+					sprite_2d.animation = "Caveman_run"
+				else:
+					sprite_2d.animation = "Caveman_idle"
+			GameManager.PossiblePowers.DINORIDER:
+				sprite_2d.animation = "Dino_Riding"
+	# Add the gravity.
+	
+	if is_on_wall_only() and not sprite_2d.animation == "Dino_Riding" and ((direction < 0 and sprite_2d.flip_h) or (direction > 0 and not sprite_2d.flip_h)):
+		has_walljump = true
+	else:
+		has_walljump = false
+		
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		if velocity.y >= 1000:
+			velocity.y = 1000
+		if velocity.y >= 300 and wallslide:
+			velocity.y = 300
+	else:
+		if not sprite_2d.animation == "Dino_Riding":
+			has_backwards_walljump = true
+		else:
+			has_backwards_walljump = false
+	
+	
+	if power_state != GameManager.PossiblePowers.NORMAL and Input.is_action_just_pressed("action"):
+		if power_state == GameManager.PossiblePowers.PIRATE and can_shoot:
+			shoot_bullet()
+		if power_state == GameManager.PossiblePowers.CAVEMAN and not does_spcial_action:
+			club_slash()
+	
+	# Handle jump.
+	
+	if Input.is_action_just_pressed("jump") and not does_spcial_action and ((is_on_floor() or coyote_timer > 0.0) or (is_on_wall_only() and direction and (has_walljump or has_backwards_walljump))):
+		velocity.y = JUMP_VELOCITY 
+		sfx_jump.play()
+		coyote_timer = 0.0 # Reset the coyote timer after a jump
+		
+		if wallslide:
+			velocity.x = -direction * SPEED 
+			sprite_2d.flip_h = (velocity.x < 0)
+			cur_direction = direction
+			has_backwards_walljump = false
+			grace_timer = WALLJUMP_GRACE
+			
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	grace_timer = max(grace_timer - delta, 0)
+	if grace_timer == 0:
+		if sprite_2d.animation == "Dino_Riding":
+			velocity.x = move_toward(velocity.x, cur_direction * SPEED, adjusted_acceleration * delta)
+		elif (direction and not does_spcial_action):
+			velocity.x = move_toward(velocity.x, direction * SPEED, adjusted_acceleration * delta)
+		elif does_spcial_action:
+			velocity.x = move_toward(velocity.x, 0, adjusted_deceleration * delta * 0.3)
+		else:
+			velocity.x = move_toward(velocity.x, 0, adjusted_deceleration * delta)
+	move_and_slide()
+	
+	if direction != 0 and is_on_floor():
+		var isLeft = direction < 0
+		sprite_2d.flip_h = isLeft 
+		cur_direction = direction
+	
+
+	if in_portal and Input.is_action_just_pressed("up") and is_on_floor():
+		if current_portal != null:
+			current_portal.level_transition()
+			current_portal = null
+		
+	#if sprite_2d.flip_h:
+		#cur_direction = -1
+	#else:
+		#cur_direction = 1
+	
+	
+func powerStateChange(new_power_state : GameManager.PossiblePowers):
+	power_state = new_power_state
+	Global.cur_power = new_power_state
+	print("PowerUp")
+
+
+
+func shoot_bullet():
+	var BulletShotScene = load("res://Objekte/bullet_shot.tscn")
+	var Bullet = BulletShotScene.instantiate()
+	Bullet.global_position = global_position
+	
+	Bullet.direction = cur_direction
+	get_parent().add_child(Bullet)
+	sprite_2d.animation = "Pirate_shoot"
+	does_moveable_action = true
+	can_shoot = false
+	$gun_out_timer.start()
+	
+func club_slash():
+	$hitbox_Timer_Bonk.start()
+	does_spcial_action = true
+	sprite_2d.animation = "BigBonk"
+	sprite_2d.play()
+	
+func _on_sprite_2d_animation_finished():
+	if sprite_2d.animation == "BigBonk" or sprite_2d.animation == "Zapped":
+		does_spcial_action = false
+		if sprite_2d.animation == "Zapped":
+			sprite_2d.animation = "default"
+		elif sprite_2d.animation == "BigBonk":
+			sprite_2d.animation = "Caveman_idle"
+		sprite_2d.play()
+
+func _on_hitbox_timer_bonk_timeout():
+	var SlashScene = load("res://Objekte/Slash.tscn")
+	var Slash = SlashScene.instantiate()
+	Slash.global_position = global_position
+	Slash.direction = cur_direction
+	get_parent().add_child(Slash)
+
+func _on_gun_out_timer_timeout():
+	does_moveable_action = false
+	can_shoot = true
+
+func Zap_powerup():
+	print("Zapped")
+	does_spcial_action = true
+	powerStateChange(GameManager.PossiblePowers.NORMAL)
+	sprite_2d.animation = "Zapped"
+	sprite_2d.play()
+
+func set_direction(new_direction : int):
+	if new_direction in [-1, 0, 1]:
+		direction = new_direction
+	else:
+		print("Invalid direction. Must be -1, 0, or 1.")
+	
